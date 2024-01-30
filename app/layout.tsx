e195c193e91b8da5e6d2eecec43764ab81a026ca
Abhijeet Raj <abhijeetraj900@gmail.com>
2024-01-30 16:46:29 +0530

    Add AuthLayout and sign-in/sign-up routes with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Discord",
-  description: "Discord is a voice, video and text communication service to talk and hang out with your friends and communities.",
+  description:
+    "Discord is a voice, video and text communication service to talk and hang out with your friends and communities.",
 };
 
 export default function RootLayout({
@@ -15,8 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={font.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={font.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
