8d06ecbcd8fe254e3c08dd1805b2dffbdf2cb9e5
Abhijeet Raj <abhijeetraj900@gmail.com>
2024-01-31 23:40:57 +0530

    Add theme provider and mode toggle components for dark mode integration

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,14 +1,15 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Next.js</h1>
-      <Image
-        src="/images/nextjs.png"
-        alt="Next.js Logo"
-        width={200}
-        height={160}
+      
+      <UserButton
+      afterSignOutUrl=""
+      />
+      <ModeToggle
       />
     </div>
   );
