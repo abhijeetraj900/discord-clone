9ceabd9aa08602b4210248be95ff6a38996cdf43
Abhijeet Raj <abhijeetraj900@gmail.com>
2024-04-03 21:46:04 +0530

    Add "deleteServer" modal type and corresponding functionality

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -1,17 +1,45 @@
-import { currentProfile } from "@/lib/current-profile";
-import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-export async function PTACH(
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+export async function DELETE(
   req: Request,
   { params }: { params: { serverId: string } }
 ) {
   try {
     const profile = await currentProfile();
-    const { name , imageUrl } = await req.json();
+
     if (!profile) {
-      return new Response(null, { status: 401 });
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const server = await db.server.delete({
+      where: {
+        id: params.serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[SERVER_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { serverId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { name, imageUrl } = await req.json();
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
     const server = await db.server.update({
       where: {
         id: params.serverId,
@@ -25,7 +53,7 @@ export async function PTACH(
 
     return NextResponse.json(server);
   } catch (error) {
-    console.log(error);
-    return new NextResponse("internal error", { status: 500 });
+    console.log("[SERVER_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
   }
 }
--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import axios from "axios";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect } from "react";
 
 import {
   Dialog,
@@ -25,26 +27,25 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
-import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
-import { useEffect } from "react";
 
 const formSchema = z.object({
-  name: z.string().min(1, { message: " Server name is required" }),
-  imageUrl: z.string().min(1, { message: "Server image is required" }),
+  name: z.string().min(1, {
+    message: "Server name is required.",
+  }),
+  imageUrl: z.string().min(1, {
+    message: "Server image is required.",
+  }),
 });
 
 export const EditServerModal = () => {
-
-  const  { isOpen, onClose, type, data } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
 
   const isModalOpen = isOpen && type === "editServer";
   const { server } = data;
 
-  const router = useRouter();
-
-  
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -54,12 +55,11 @@ export const EditServerModal = () => {
   });
 
   useEffect(() => {
-
-    if(server){
+    if (server) {
       form.setValue("name", server.name);
       form.setValue("imageUrl", server.imageUrl);
     }
-  },[server, form]);
+  }, [server, form]);
 
   const isLoading = form.formState.isSubmitting;
 
@@ -78,24 +78,22 @@ export const EditServerModal = () => {
   const handleClose = () => {
     form.reset();
     onClose();
-  }
-
- 
+  };
 
   return (
-    <Dialog open = {isModalOpen} onOpenChange={handleClose}>
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Custom server
+            Customize your server
           </DialogTitle>
-          <DialogDescription className=" text-center text-zinc-500">
-            discription
+          <DialogDescription className="text-center text-zinc-500">
+            Give your server a personality with a name and an image. You can
+            always change it later.
           </DialogDescription>
         </DialogHeader>
-
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className=" space-y-8">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
             <div className="space-y-8 px-6">
               <div className="flex items-center justify-center text-center">
                 <FormField
@@ -114,20 +112,20 @@ export const EditServerModal = () => {
                   )}
                 />
               </div>
+
               <FormField
                 control={form.control}
                 name="name"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                      Server Name
+                      Server name
                     </FormLabel>
                     <FormControl>
                       <Input
                         disabled={isLoading}
-                        className="bg-zinc-300/50 border-0 focus-visible:ring-0
-                           text-black focus-visible:ring-offset-0"
-                        placeholder="Server Name"
+                        className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                        placeholder="Enter server name"
                         {...field}
                       />
                     </FormControl>
@@ -138,7 +136,7 @@ export const EditServerModal = () => {
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
               <Button variant="primary" disabled={isLoading}>
-                Create
+                Save
               </Button>
             </DialogFooter>
           </form>
