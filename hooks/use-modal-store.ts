f90ff27439806436b3da00f09104d06d11a4ebfd
Abhijeet Raj <abhijeetraj900@gmail.com>
2024-04-07 22:29:48 +0530

    Refactor use-modal-store.ts and navigation components

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,18 +1,7 @@
-import { create } from "zustand";
 import { Channel, ChannelType, Server } from "@prisma/client";
+import { create } from "zustand";
 
-export type ModalType =
-  | "createServer"
-  | "invite"
-  | "editServer"
-  | "members"
-  | "createChannel"
-  | "leaveServer"
-  | "deleteServer"
-  | "deleteChannel"
-  | "editChannel"
-  | "messageFile"
-  | "deleteMessage";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer" | "deleteChannel" | "editChannel" | "messageFile" | "deleteMessage";
 
 interface ModalData {
   server?: Server;
@@ -34,6 +23,6 @@ export const useModal = create<ModalStore>((set) => ({
   type: null,
   data: {},
   isOpen: false,
-  onOpen: (type, data) => set({ isOpen: true, type, data }),
-  onClose: () => set({ type: null, isOpen: false }),
+  onOpen: (type, data = {}) => set({ isOpen: true, type, data }),
+  onClose: () => set({ type: null, isOpen: false })
 }));
